0f1da3297a9d5ad9df035c6294df868a5133808b
Bradelut <brandon_v_dijk@outlook.com>
2024-03-06 12:38:43 +0100

    Create disablehello.ps1

---
+++ DeviceScripts/disablehello.ps1
@@ -0,0 +1,10 @@
+﻿#Delete existing pins
+$passportFolder = "C:\Windows\ServiceProfiles\LocalService\AppData\Local\Microsoft\Ngc"
+
+if(Test-Path -Path $passportFolder)
+{
+Takeown /f $passportFolder /r /d "Y"
+ICACLS $passportFolder /reset /T /C /L /Q
+
+Remove-Item –path $passportFolder –recurse -force
+}
